fdaeac6a1b25e29c5809a9acaf797c31ffc148b7
ViTr <ViTr@list.ru>
2017-12-02 12:51:10 +0400

    feat: Улучшения функциональности и мелкий рефакторинг

--- src/ru/otus/servlet/TotalizatorServlet.java
+++ src/ru/otus/servlet/TotalizatorServlet.java
@@ -1,13 +1,18 @@
 package ru.otus.servlet;
 
 import static ru.otus.TotalizatorConstant.WINNER_REQUEST_ATTRIBUTE;
-import static ru.otus.util.EmailRandomizer.*;
+import static ru.otus.util.EmailRandomizer.getWinnerCount;
+import static ru.otus.util.EmailRandomizer.getWinnersAsList;
+import static ru.otus.util.EmailRandomizer.saveObfuscatedEmailsToFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,14 +25,22 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
- * Servlet implementation class FileUploadServlet
+ * Servlet implementation of Totalizator
  */
 @WebServlet("/result")
-public class FileUploadServlet extends HttpServlet {
+public class TotalizatorServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 5885899233778066005L;
 	
-	private final static int WINNER_COUNT = 2;
+	/**
+	 * Output files with obfuscated emails
+	 */
+	private final static String DESTINATION_FILE = "obfuscatedEmails.txt";
+	
+	/**
+	 * Congratulation JSP
+	 */
+	private final static String WINNER_JSP = "WEB-INF/jsp/winners.jsp";
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -37,18 +50,17 @@ public class FileUploadServlet extends HttpServlet {
 	      // Создание обработчика загрузки файла
 	      ServletFileUpload upload = new ServletFileUpload(factory);
 	      try {
-	        // Последовательный upload
-	        List<FileItem> items = upload.parseRequest(request);
-	        FileItem file = items.stream().findFirst().get();
-	        List<String> allLines = new ArrayList<>();
-	        try (Scanner scanner = new Scanner(file.getInputStream())) {
-	        	while(scanner.hasNextLine()) {
-	        		allLines.add(scanner.nextLine());
-	        	}
+	        FileItem file = (FileItem) upload.parseRequest(request).stream().findFirst().get();
+	        List<String> allLines;
+	        // Scanner тоже неплох :)
+	        try (BufferedReader buffer = new BufferedReader(
+	        		new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8))) {
+	            allLines = buffer.lines().collect(Collectors.toList());
 	        }
-	        saveToFileObfuscatedEmails(request.getServletContext().getRealPath("obfuscatedEmails.txt"), allLines);
-	        request.setAttribute(WINNER_REQUEST_ATTRIBUTE, getWinnersAsList(allLines, WINNER_COUNT));
-	        request.getRequestDispatcher("WEB-INF/jsp/winners.jsp").forward(request, response); 
+	        ServletContext context = getServletContext();
+	        allLines = saveObfuscatedEmailsToFile(context.getRealPath(DESTINATION_FILE), allLines);
+	        request.setAttribute(WINNER_REQUEST_ATTRIBUTE, getWinnersAsList(allLines, getWinnerCount(context)));
+	        request.getRequestDispatcher(WINNER_JSP).forward(request, response); 
 	      }
 	      catch(Exception e){
 	    	  e.printStackTrace();
--- src/ru/otus/util/EmailRandomizer.java
+++ src/ru/otus/util/EmailRandomizer.java
@@ -3,13 +3,20 @@ package ru.otus.util;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Comparator;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import javax.servlet.ServletContext;
 
 public class EmailRandomizer {
 	
 	private static final String EMAIL_SEPARATOR = "@";
+
+	private final static String WINNER_COUNT_INIT_PARAM = "winnerCount";
 
 	public static Stream<String> sweepstake(List<String> emails, int winnerCount) throws IOException {
 		return new EmailCollection(emails).randomEmails(winnerCount);
@@ -19,11 +26,18 @@ public class EmailRandomizer {
 		return sweepstake(emails, winnerCount).collect(Collectors.joining(", "));
 	}
 	
-	public static void saveToFileObfuscatedEmails(String fileName, List<String> emails) throws IOException {
+	public static List<String> saveObfuscatedEmailsToFile(String fileName, List<String> emails) throws IOException {
 		List<String> obfuscatedEmails = emails.stream()
 			    .map(email -> email.contains(EMAIL_SEPARATOR) ? email.split(EMAIL_SEPARATOR)[0] : email)
+			    .sorted(Comparator.comparing(String::hashCode))
 			    .collect(Collectors.toList());
 		
-		Files.write(Paths.get(fileName), obfuscatedEmails);
+		Files.write(Paths.get(fileName), obfuscatedEmails, StandardOpenOption.CREATE_NEW);
+		
+		return obfuscatedEmails;
+	}
+	
+	public static int getWinnerCount(ServletContext context) {
+		return Integer.decode(context.getInitParameter(WINNER_COUNT_INIT_PARAM));
 	}
 }
